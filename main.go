80578f6947e947b3ce422d6b2e9067816468bc38
yongkang <zhangyongkang@lanxiniu.com>
2016-10-11 00:30:33 +0800

    settings

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
 	"os"
 
+	"time"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang/glog"
+	"gopkg.in/mgo.v2"
 )
 
 // var CREATE_TABLE = `CREATE TABLE `userinfo` (
@@ -15,14 +20,25 @@ import (
 //     `created` DATE NULL DEFAULT NULL,
 //     PRIMARY KEY (`uid`)
 // )`
+var (
+	mysqlLink = "messageboard_mysql_1"
+	mysqlPort = "3307"
+	mysqlUser = "root"
+	mysqlPWD  = "root"
+
+	mongoLink = "messageboard_mongodb_1"
+	mongoPort = "27017"
+)
 
 func testMySQL() {
-	dbURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", "root", "root", os.Getenv("MYSQL_PORT_3306_TCP_ADDR"), os.Getenv("MYSQL_PORT_3306_TCP_PORT"), os.Getenv("USERS_DB_NAME"))
-	fmt.Println(dbURI)
-	db, err := sql.Open("mysql", dbURI)
+	// dbURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/", "root", "root", os.Getenv("MYSQL_PORT_3306_TCP_ADDR"), os.Getenv("MYSQL_PORT_3306_TCP_PORT"))
+	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/", mysqlUser, mysqlPWD, mysqlLink, mysqlPort)
+	glog.Infoln("mysqlURI: ", mysqlURI)
+	db, err := sql.Open("mysql", mysqlURI)
 	if err != nil {
 		panic("Open error")
 	}
+	glog.Infoln("link to mysql sucecess")
 	defer db.Close()
 	// dbName := "test"
 	// _, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + dbName)
@@ -63,6 +79,19 @@ func testMySQL() {
 	// }
 }
 
+func testMongo() {
+
+	// dbURI := fmt.Sprintf("%s:%s", os.Getenv("MONGO_PORT_27017_TCP_ADDR"), os.Getenv("MONGO_PORT_27017_TCP_PORT"))
+	session, err := mgo.DialWithTimeout(fmt.Sprintf("%s:%s", mongoLink, mongoPort), 5*time.Second)
+	if err != nil {
+		fmt.Printf("MONGO ADDRESS: %s", os.Getenv("DATABASE_PORT_27017_TCP_ADDR"))
+		fmt.Println("panic on mongo")
+		panic(err)
+	}
+	glog.Infoln("link to mongo success")
+	defer session.Close()
+}
+
 // func testMongo() {
 
 // 	// dbURI := fmt.Sprintf("%s:%s", os.Getenv("MONGO_PORT_27017_TCP_ADDR"), os.Getenv("MONGO_PORT_27017_TCP_PORT"))
@@ -76,8 +105,20 @@ func testMySQL() {
 // }
 
 func main() {
+	flag.Lookup("logtostderr").Value.Set("true")
+	flag.Parse()
 	testMySQL()
+	testMongo()
 	for true {
-
+		glog.Infoln("runing...")
+		time.Sleep(time.Second * 1)
 	}
 }
+
+// func main() {
+// 	testMySQL()
+// 	testMongo()
+// 	for true {
+
+// 	}
+// }
