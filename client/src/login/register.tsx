3de3138d41e80e197438edae1e069a4b7596c702
joint-song <xuguang1992@vip.qq.com>
2016-10-16 21:30:04 +0800

    add login and register page.

--- client/src/login/register.tsx
+++ client/src/login/register.tsx
@@ -6,9 +6,9 @@ export default class Register extends React.Component<{}, {}> {
             <div>
                 <span>注册</span>
                 <form action="register" method="POST">
-                    <input type="text" name="name" />
-                    <input type="password" name="pwd" />
-                    <button type="submit" value="提交"/>
+                    用户名：<input type="text" name="name" /><br/>
+                    密码：<input type="password" name="pwd" /><br/>
+                    <input type="submit" value="提交" /><br/>
                 </form>
             </div>
         );
